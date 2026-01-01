fb8bcbac96d2fc4b448bf89b551563409d5878b4
Angelos <angelos353@gmail.com>
2026-01-01 17:28:55 +0200

    improved plotting functions

--- src/plotting.py
+++ src/plotting.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-import torch
 
 from .constants import PIECE_TO_STR, NUM_SETUP_ROWS, NUM_SETUP_COLUMNS, NUM_SETUP_SQUARES
 
@@ -22,14 +21,11 @@ def plot_setup(setup, tile_size=0.6, font_size=15, piece_labels=PIECE_TO_STR, ax
                                  linewidth=1, edgecolor='none', facecolor='lightblue', alpha=0.5)
         ax.add_patch(rect)
 
-    setup = np.squeeze(setup)
+    setup = np.atleast_2d(setup)
 
-    for square in range(min(len(setup), NUM_SETUP_SQUARES)):
+    for square in range(setup.shape[1]):
         row, column = divmod(square, NUM_SETUP_COLUMNS)
-        piece = setup[square]
-        if isinstance(setup, torch.Tensor):
-            piece = piece.item()
-        ax.text(column + 0.5, NUM_SETUP_ROWS - row - 0.5, piece_labels[piece],
+        ax.text(column + 0.5, NUM_SETUP_ROWS - row - 0.5, piece_labels[setup[0, square]],
                 ha='center', va='center', fontsize=font_size)
 
     ax.set_xticks(range(NUM_SETUP_COLUMNS + 1))
@@ -49,27 +45,29 @@ def plot_setups(setups, num_columns=3, tile_size=0.6, font_size=15,
                 piece_labels=PIECE_TO_STR, w_space=0.1, h_space=0.1):
 
     fig, axes = create_plot_grid(len(setups), num_columns,
-                               NUM_SETUP_COLUMNS * tile_size, NUM_SETUP_ROWS * tile_size)
+                                 NUM_SETUP_COLUMNS * tile_size, NUM_SETUP_ROWS * tile_size)
     fig.subplots_adjust(wspace=w_space, hspace=h_space)
+    axes = axes.ravel()
 
-    for i in range(len(setups)):
-        row, column = divmod(i, num_columns)
-        plot_setup(setups[i], tile_size=tile_size, font_size=font_size,
-                   piece_labels=piece_labels, ax=axes[row][column])
+    for setup, ax in zip(setups, axes):
+        plot_setup(setup, tile_size=tile_size, font_size=font_size,
+                   piece_labels=piece_labels, ax=ax)
 
-    for i in range(len(setups), len(fig.axes)):
-        row, column = divmod(i, num_columns)
-        axes[row][column].axis('off')
+    for i in range(len(setups), len(axes)):
+        axes[i].axis('off')
 
     plt.show()
 
 
-def plot_setup_generation(setup, distributions, width=4, height=2):
+def plot_setup_generation(setup, distributions, width=4, height=2, adjust_y_lim=False):
     fig, axes = create_plot_grid(2*(NUM_SETUP_SQUARES + 1), 2, width, height)
+    setup = setup.squeeze()
     for square in range(NUM_SETUP_SQUARES):
         plot_setup(setup[:square], ax=axes[square][0])
         axes[square][1].set_title("Probability distribution for next piece")
         axes[square][1].bar(PIECE_TO_STR.values(), distributions[square])
+        if not adjust_y_lim:
+            axes[square][1].set_ylim(0.0, 1.0)
     plot_setup(setup, ax=axes[NUM_SETUP_SQUARES][0])
     axes[NUM_SETUP_SQUARES][1].axis('off')
     fig.tight_layout()
